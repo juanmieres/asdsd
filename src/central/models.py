5af1c024f21344e37caf336a720e11e73466899d
Juan Mieres <juan.mieres.s@gmail.com>
2024-02-27 18:20:33 -0300

    update

---
+++ src/central/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+
+#     # atributos
+#     id_fiscal = models.CharField(max_length=255, blank=True, null=True)
+#     # suscriptor = models.BooleanField(default=False)
+#     # id_respaldo = models.IntegerField(null=True, blank=True) # TODO: eliminar cuando se migre
+
+#     # atributos (relacionados)
+
+#     class Meta:
+#         ordering = ['id']
+#         verbose_name = 'Usuario'
+#         verbose_name_plural = 'Usuarios'
+
+#     def __str__(self):
+#         return self.username.upper()
+
